1a705cdc8ffc1ead009ab49caefcfaaea114a273
Sanni David <sannidavidsmart@gmail.com>
2025-04-02 02:52:21 +0100

    feat: setup admionlogin v1.4

--- database/seeders/AssetSeeder.php
+++ database/seeders/AssetSeeder.php
@@ -34,69 +34,67 @@ class AssetSeeder extends Seeder
         // ];
 
         $symbols = [
-
-            'PLTR'
-            // // Technology (50)
-            // 'AAPL', 'MSFT', 'GOOGL', 'AMZN', 'NVDA', 'META', 'TSLA', 'AVGO', 'ASML', 'ADBE',
-            // 'CSCO', 'ORCL', 'CRM', 'INTC', 'AMD', 'QCOM', 'TXN', 'IBM', 'NOW', 'SNOW',
-            // 'PANW', 'UBER', 'NET', 'SHOP', 'CRWD', 'ZS', 'MDB', 'DDOG', 'TEAM', 'FTNT',
-            // 'WDAY', 'ADSK', 'INTU', 'PYPL', 'SQ', 'DOCU', 'ZM', 'ROKU', 'SPLK', 'OKTA',
-            // 'CDNS', 'ANSS', 'KLAC', 'LRCX', 'AMAT', 'MU', 'MRVL', 'ADI', 'NXPI', 'SWKS',
+            // Technology (50)
+            'AAPL', 'MSFT', 'GOOGL', 'AMZN', 'NVDA', 'META', 'TSLA', 'AVGO', 'ASML', 'ADBE',
+            'CSCO', 'ORCL', 'CRM', 'INTC', 'AMD', 'QCOM', 'TXN', 'IBM', 'NOW', 'SNOW',
+            'PANW', 'UBER', 'NET', 'SHOP', 'CRWD', 'ZS', 'MDB', 'DDOG', 'TEAM', 'FTNT',
+            'WDAY', 'ADSK', 'INTU', 'PYPL', 'SQ', 'DOCU', 'ZM', 'ROKU', 'SPLK', 'OKTA',
+            'CDNS', 'ANSS', 'KLAC', 'LRCX', 'AMAT', 'MU', 'MRVL', 'ADI', 'NXPI', 'SWKS', 'PLTR',
         
-            // // Financials (30)
-            // 'JPM', 'BAC', 'WFC', 'C', 'GS', 'MS', 'SCHW', 'BLK', 'AXP', 'PYPL',
-            // 'V', 'MA', 'DFS', 'COF', 'USB', 'TFC', 'PNC', 'BK', 'STT', 'ICE',
-            // 'CME', 'SPGI', 'MCO', 'FIS', 'FISV', 'GPN', 'AJG', 'MMC', 'TW', 'NDAQ',
+            // Financials (30)
+            'JPM', 'BAC', 'WFC', 'C', 'GS', 'MS', 'SCHW', 'BLK', 'AXP', 'PYPL',
+            'V', 'MA', 'DFS', 'COF', 'USB', 'TFC', 'PNC', 'BK', 'STT', 'ICE',
+            'CME', 'SPGI', 'MCO', 'FIS', 'FISV', 'GPN', 'AJG', 'MMC', 'TW', 'NDAQ',
         
-            // // Healthcare (30)
-            // 'UNH', 'JNJ', 'PFE', 'ABBV', 'MRK', 'LLY', 'TMO', 'DHR', 'AMGN', 'GILD',
-            // 'BMY', 'VRTX', 'REGN', 'ISRG', 'MDT', 'SYK', 'BDX', 'ZTS', 'CI', 'HUM',
-            // 'ELV', 'CVS', 'ANTM', 'IQV', 'EW', 'IDXX', 'DXCM', 'BSX', 'HCA', 'MRNA',
+            // Healthcare (30)
+            'UNH', 'JNJ', 'PFE', 'ABBV', 'MRK', 'LLY', 'TMO', 'DHR', 'AMGN', 'GILD',
+            'BMY', 'VRTX', 'REGN', 'ISRG', 'MDT', 'SYK', 'BDX', 'ZTS', 'CI', 'HUM',
+            'ELV', 'CVS', 'ANTM', 'IQV', 'EW', 'IDXX', 'DXCM', 'BSX', 'HCA', 'MRNA',
         
-            // // Consumer Discretionary (25)
-            // 'HD', 'LOW', 'NKE', 'MCD', 'SBUX', 'TGT', 'COST', 'BKNG', 'MAR', 'HLT',
-            // 'LVS', 'YUM', 'CMG', 'DPZ', 'NFLX', 'DIS', 'RCL', 'CCL', 'EXPE', 'ABNB',
-            // 'DHI', 'LEN', 'NVR', 'PHM', 'TSCO',
+            // Consumer Discretionary (25)
+            'HD', 'LOW', 'NKE', 'MCD', 'SBUX', 'TGT', 'COST', 'BKNG', 'MAR', 'HLT',
+            'LVS', 'YUM', 'CMG', 'DPZ', 'NFLX', 'DIS', 'RCL', 'CCL', 'EXPE', 'ABNB',
+            'DHI', 'LEN', 'NVR', 'PHM', 'TSCO',
         
-            // // Industrials (25)
-            // 'HON', 'GE', 'CAT', 'BA', 'RTX', 'LMT', 'GD', 'NOC', 'DE', 'UPS',
-            // 'FDX', 'CSX', 'UNP', 'NSC', 'WM', 'RSG', 'WAB', 'EMR', 'ETN', 'ITW',
-            // 'ROK', 'SWK', 'PH', 'DOV', 'AME',
+            // Industrials (25)
+            'HON', 'GE', 'CAT', 'BA', 'RTX', 'LMT', 'GD', 'NOC', 'DE', 'UPS',
+            'FDX', 'CSX', 'UNP', 'NSC', 'WM', 'RSG', 'WAB', 'EMR', 'ETN', 'ITW',
+            'ROK', 'SWK', 'PH', 'DOV', 'AME',
         
-            // // Consumer Staples (20)
-            // 'PG', 'KO', 'PEP', 'WMT', 'COST', 'PM', 'MO', 'MDLZ', 'KHC', 'CL',
-            // 'EL', 'GIS', 'KMB', 'SYY', 'HSY', 'ADM', 'STZ', 'BF.B', 'CPB', 'MNST',
+            // Consumer Staples (20)
+            'PG', 'KO', 'PEP', 'WMT', 'COST', 'PM', 'MO', 'MDLZ', 'KHC', 'CL',
+            'EL', 'GIS', 'KMB', 'SYY', 'HSY', 'ADM', 'STZ', 'BF.B', 'CPB', 'MNST',
         
-            // // Energy (15)
-            // 'XOM', 'CVX', 'COP', 'EOG', 'SLB', 'OXY', 'PSX', 'MPC', 'VLO', 'PXD',
-            // 'HES', 'DVN', 'FANG', 'HAL', 'BKR',
+            // Energy (15)
+            'XOM', 'CVX', 'COP', 'EOG', 'SLB', 'OXY', 'PSX', 'MPC', 'VLO', 'PXD',
+            'HES', 'DVN', 'FANG', 'HAL', 'BKR',
         
-            // // Utilities (10)
-            // 'NEE', 'DUK', 'SO', 'D', 'AEP', 'EXC', 'SRE', 'PEG', 'ED', 'EIX',
+            // Utilities (10)
+            'NEE', 'DUK', 'SO', 'D', 'AEP', 'EXC', 'SRE', 'PEG', 'ED', 'EIX',
         
-            // // Real Estate (10)
-            // 'AMT', 'PLD', 'CCI', 'EQIX', 'PSA', 'SBAC', 'DLR', 'WELL', 'AVB', 'O',
+            // Real Estate (10)
+            'AMT', 'PLD', 'CCI', 'EQIX', 'PSA', 'SBAC', 'DLR', 'WELL', 'AVB', 'O',
         
-            // // Materials (10)
-            // 'LIN', 'APD', 'SHW', 'FCX', 'NEM', 'DOW', 'ECL', 'PPG', 'VMC', 'NUE',
+            // Materials (10)
+            'LIN', 'APD', 'SHW', 'FCX', 'NEM', 'DOW', 'ECL', 'PPG', 'VMC', 'NUE',
         
-            // // Communication Services (15)
-            // 'GOOG', 'GOOGL', 'META', 'DIS', 'NFLX', 'T', 'VZ', 'TMUS', 'CHTR', 'CMCSA',
-            // 'EA', 'TTWO', 'ATVI', 'ROKU', 'ZG',
+            // Communication Services (15)
+            'GOOG', 'GOOGL', 'META', 'DIS', 'NFLX', 'T', 'VZ', 'TMUS', 'CHTR', 'CMCSA',
+            'EA', 'TTWO', 'ATVI', 'ROKU', 'ZG',
         
-            // // Top 50 by market cap
-            // 'BTCUSD', 'ETHUSD', 'USDTUSD', 'BNBUSD', 'SOLUSD', 'XRPUSD', 'USDCUSD', 'ADAUSD', 'DOGEUSD', 'AVAXUSD',
-            // 'SHIBUSD', 'DOTUSD', 'TRXUSD', 'LINKUSD', 'MATICUSD', 'WBTCUSD', 'TONUSD', 'ICPUSD', 'DAIUSD', 'LTCUSD',
-            // 'BCHUSD', 'UNIUSD', 'ATOMUSD', 'XLMUSD', 'ETCUSD', 'INJUSD', 'XMRUSD', 'FILUSD', 'IMXUSD', 'APTUSD',
-            // 'RNDRUSD', 'STXUSD', 'HBARUSD', 'CROUSD', 'NEARUSD', 'VETUSD', 'OPUSD', 'MKRUSD', 'GRTUSD', 'ARBUSD',
-            // 'THETAUSD', 'FDUSD', 'PEPEUSD', 'KASUSD', 'RUNEUSD', 'FRAXUSD', 'AAVEUSD', 'ALGOUSD', 'FLOWUSD', 'EGLDUSD',
+            // Top 50 by market cap
+            'BTCUSD', 'ETHUSD', 'USDTUSD', 'BNBUSD', 'SOLUSD', 'XRPUSD', 'USDCUSD', 'ADAUSD', 'DOGEUSD', 'AVAXUSD',
+            'SHIBUSD', 'DOTUSD', 'TRXUSD', 'LINKUSD', 'MATICUSD', 'WBTCUSD', 'TONUSD', 'ICPUSD', 'DAIUSD', 'LTCUSD',
+            'BCHUSD', 'UNIUSD', 'ATOMUSD', 'XLMUSD', 'ETCUSD', 'INJUSD', 'XMRUSD', 'FILUSD', 'IMXUSD', 'APTUSD',
+            'RNDRUSD', 'STXUSD', 'HBARUSD', 'CROUSD', 'NEARUSD', 'VETUSD', 'OPUSD', 'MKRUSD', 'GRTUSD', 'ARBUSD',
+            'THETAUSD', 'FDUSD', 'PEPEUSD', 'KASUSD', 'RUNEUSD', 'FRAXUSD', 'AAVEUSD', 'ALGOUSD', 'FLOWUSD', 'EGLDUSD',
             
-            // // Next 50 (51-100)
-            // 'QNTUSD', 'BSVUSD', 'XTZUSD', 'EOSUSD', 'MINAUSD', 'AXSUSD', 'SANDUSD', 'MANAUSD', 'NEOUSD', 'KCSUSD',
-            // 'BTTUSD', 'CHZUSD', 'USDPUSD', 'SNXUSD', 'FTMUSD', 'BGBUSD', 'CRVUSD', 'GALAUSD', 'ROSEUSD', 'ZECUSD',
-            // 'XECUSD', 'KAVAUSD', 'DASHUSD', 'PAXGUSD', 'IOTAUSD', 'WEMIXUSD', 'COMPUSD', 'HNTUSD', 'CAKEUSD', 'GMXUSD',
-            // 'CFXUSD', 'TUSDUSD', 'BONKUSD', 'GTUSD', '1INCHUSD', 'LDOUSD', 'XDCUSD', 'FXSUSD', 'SUIUSD', 'APEUSD',
-            // 'ENSUSD', 'AGIXUSD', 'RPLUSD', 'OCEANUSD', 'NEXOUSD', 'ZILUSD', 'KLAYUSD', 'GNOUSD', 'YFIUSD', 'WOOUSD',
+            // Next 50 (51-100)
+            'QNTUSD', 'BSVUSD', 'XTZUSD', 'EOSUSD', 'MINAUSD', 'AXSUSD', 'SANDUSD', 'MANAUSD', 'NEOUSD', 'KCSUSD',
+            'BTTUSD', 'CHZUSD', 'USDPUSD', 'SNXUSD', 'FTMUSD', 'BGBUSD', 'CRVUSD', 'GALAUSD', 'ROSEUSD', 'ZECUSD',
+            'XECUSD', 'KAVAUSD', 'DASHUSD', 'PAXGUSD', 'IOTAUSD', 'WEMIXUSD', 'COMPUSD', 'HNTUSD', 'CAKEUSD', 'GMXUSD',
+            'CFXUSD', 'TUSDUSD', 'BONKUSD', 'GTUSD', '1INCHUSD', 'LDOUSD', 'XDCUSD', 'FXSUSD', 'SUIUSD', 'APEUSD',
+            'ENSUSD', 'AGIXUSD', 'RPLUSD', 'OCEANUSD', 'NEXOUSD', 'ZILUSD', 'KLAYUSD', 'GNOUSD', 'YFIUSD', 'WOOUSD',
             
             // // Next 50 (101-150)
             // 'CELOUSD', 'DYDXUSD', 'TFUELUSD', 'JSTUSD', 'IOTXUSD', 'ANKRUSD', 'ASTRUSD', 'GUSDUSD', 'SKLUSD', 'CSPRUSD',
@@ -113,11 +111,11 @@ class AssetSeeder extends Seeder
             // 'PHAUSD', 'FLOKIUSD', 'MOVRUSD', 'TUSD', 'CFGUSD', 'AERGOUSD', 'BICOUSD', 'TRUUSD', 'MXCUSD', 'ALPHAUSD',
             
             // Next 50 (201-250)
-            // 'AIOZUSD', 'MBOXUSD', 'AURORAUSD', 'SUNUSD', 'RDNTUSD', 'BELUSD', 'RADUSD', 'CTXCUSD', 'VRAUSD', 'BUSD',
-            // 'HIGHUSD', 'EDENUSD', 'FIDAUSD', 'TLMUSD', 'QUICKUSD', 'XNOUSD', 'AKTUSD', 'MLNUSD', 'REPUSD', 'ASTUSD',
-            // 'BTRSTUSD', 'GHSTUSD', 'MNGOUSD', 'RAREUSD', 'PROUSD', 'OUSD', 'LCXUSD', 'FARMUSD', 'POLSUSD', 'ALICEUSD',
-            // 'FORTHUSD', 'KP3RUSD', 'BADGERUSD', 'BONDUSD', 'TRBUSD', 'IDEXUSD', 'TRIBEUSD', 'GTCUSD', 'MIRUSD', 'ASMUSD',
-            // 'CLVUSD', 'DFIUSD', 'FUNUSD', 'GUSD', 'MULTIUSD', 'NESTUSD', 'PLAUSD', 'PROMUSD', 'SUKUUSD', 'VELOUSD'
+            'AIOZUSD', 'MBOXUSD', 'AURORAUSD', 'SUNUSD', 'RDNTUSD', 'BELUSD', 'RADUSD', 'CTXCUSD', 'VRAUSD', 'BUSD',
+            'HIGHUSD', 'EDENUSD', 'FIDAUSD', 'TLMUSD', 'QUICKUSD', 'XNOUSD', 'AKTUSD', 'MLNUSD', 'REPUSD', 'ASTUSD',
+            'BTRSTUSD', 'GHSTUSD', 'MNGOUSD', 'RAREUSD', 'PROUSD', 'OUSD', 'LCXUSD', 'FARMUSD', 'POLSUSD', 'ALICEUSD',
+            'FORTHUSD', 'KP3RUSD', 'BADGERUSD', 'BONDUSD', 'TRBUSD', 'IDEXUSD', 'TRIBEUSD', 'GTCUSD', 'MIRUSD', 'ASMUSD',
+            'CLVUSD', 'DFIUSD', 'FUNUSD', 'GUSD', 'MULTIUSD', 'NESTUSD', 'PLAUSD', 'PROMUSD', 'SUKUUSD', 'VELOUSD'
         ];
         
         $apiKey = env('ASSET_KEY');;
