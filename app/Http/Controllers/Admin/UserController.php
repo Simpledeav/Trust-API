666732e0d280cbc2cd2daeca0f45012e0c308593
Sanni David <sannidavidsmart@gmail.com>
2025-04-09 18:35:19 +0100

    feat: update admin v1.1

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -19,7 +19,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::paginate(20);
+        $users = User::orderBy('created_at', 'desc')->paginate(20);
 
         return view('admin.user', [
             'users' => $users
